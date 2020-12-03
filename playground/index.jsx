99044fd0cfcb0ab6f69cf6314218141e09c0868b
Ezio Lin <ninesuns.lin@outlook.com>
2020-12-03 14:25:54 +0800

    fix: 项目无法直接启动的问题

--- playground/index.jsx
+++ playground/index.jsx
@@ -1,34 +1,7 @@
+import '../dist/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import BraftEditor from '../src'
-import ColorPicker from 'braft-extensions/dist/color-picker'
-import Table from 'braft-extensions/dist/table'
-import CodeHighlighter from 'braft-extensions/dist/code-highlighter'
-import Emoticon, { defaultEmoticons } from 'braft-extensions/dist/emoticon'
-
-import 'braft-extensions/dist/emoticon.css'
-import 'braft-extensions/dist/color-picker.css'
-import 'braft-extensions/dist/table.css'
-import 'braft-extensions/dist/code-highlighter.css'
-
-const emoticons = defaultEmoticons.map(item => require(`braft-extensions/dist/assets/${item}`))
-
-const hooks = {
-  'set-image-alignment': () => {
-    return 'left'
-  }
-}
-
-BraftEditor.use([
-  Emoticon({
-    emoticons: emoticons
-  }),
-  // ColorPicker({
-  //   theme: 'dark'
-  // }),
-  Table(),
-  CodeHighlighter()
-])
 
 class Demo extends React.Component {
 
@@ -39,7 +12,7 @@ class Demo extends React.Component {
     this.state = {
       count: 0,
       readOnly: false,
-      editorState: BraftEditor.createEditorState('<p data-foo="adasd" class="my-classname"><img src="https://www.baidu.com/img/bd_logo1.png?where=super" /><span style="color:#e25041;">asdasdasda</span>asdads</p>')
+      editorState: BraftEditor.createEditorState(null)
     }
 
   }
@@ -65,49 +38,8 @@ class Demo extends React.Component {
       <div>
         <div className="demo" id="demo">
           <BraftEditor
-            extendControls={[{
-              key: 'log-raw',
-              type: 'button',
-              text: 'Log RAW',
-              // disabled: true,
-              onClick: this.logRAW,
-            }, {
-              key: 'log-html',
-              type: 'button',
-              text: 'Log HTML',
-              // disabled: true,
-              onClick: this.logHTML,
-            }, {
-              key: 'my-modal',
-              type: 'modal',
-              text: 'modal',
-              // disabled: true,
-              modal: {
-                id: 'a',
-                closeOnBlur: true,
-                confirmable: true,
-                closeOnConfirm: false,
-                component: <div>123123</div>
-              }
-            }, {
-              key: 'my-dropdown',
-              type: 'dropdown',
-              text: 'Hello',
-              // disabled: true,
-              component: <h1>Hello World!</h1>
-            }]}
-            colors={['#e25041']}
-            headings={['header-one', 'unstyled']}
-            placeholder="Hello World!"
-            fixPlaceholder={true}
-            allowInsertLinkText={true}
-            triggerChangeOnMount={false}
             value={editorState}
             onChange={this.handleChange}
-            readOnly={readOnly}
-            hooks={hooks}
-            imageResizable={true}
-            imageEqualRatio={true}
           />
         </div>
       </div>
